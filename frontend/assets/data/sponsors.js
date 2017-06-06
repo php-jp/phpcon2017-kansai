6ef1bb1e51409a1bfe3aabe7ffae2616c42ae0f7
mikakane <t.goto@chatbox-inc.com>
2017-06-06 22:59:20 +0900

    本サイト公開 (#25)
    * hoge
    
    * hoge
    
    * hoge
    
    * Markup Pug
    
    * add images
    
    * styling SCSS
    
    * ファイル整理
    
    * remake allFile
    
    * add images
    
    * update
    
    * update
    
    * update
    
    * update
    
    * images　images2　のフォルダ名修正
    
    * map画像を変更
    
    * sec_staffをコメントアウト
    
    * $crl-font フォントカラー変更
    
    * 会場に「北館」追加
    
    * Hero_nav　フォントカラーを変更
    
    * 画像フォルダ名の変更による影響箇所の画像のパスを修正
    
    * 文言修正
    
    * font-familyの指定修正
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * fix_typo

---
+++ frontend/assets/data/sponsors.js
@@ -0,0 +1,56 @@
+module.exports = {
+    platinum: [
+        {
+            "url": "http://www.lockon.co.jp",
+            "img": "banner_platinum_01.png",
+        },
+    ],
+    gold: [
+        {
+            "url": "https://splout.co.jp/",
+            "img": "banner_gold_01.png",
+        },
+        {
+            "url": "https://levtech.jp/",
+            "img": "banner_gold_02.png",
+        },
+        {
+            "url": "https://cloudpack.jp/",
+            "img": "banner_gold_03.png",
+        },
+        {
+            "url": "https://www.firstserver.co.jp/",
+            "img": "banner_gold_04.png",
+        },
+    ],
+    silver: [
+        {
+            "url": "https://www.mercari.com/jp/",
+            "img": "banner_silver_01.png",
+        },
+        {
+            "url": "http://www.relation2012.com/",
+            "img": "banner_silver_02.png",
+        },
+        {
+            "url": "https://www.suzukishouten.co.jp/",
+            "img": "banner_silver_03.png",
+        },
+        {
+            "url": "https://10mado.jp/",
+            "img": "banner_silver_04.png",
+        },
+        {
+            "url": "https://cookbiz.jp/",
+            "img": "banner_silver_05.png",
+        },
+        {
+            "url": "https://www.phpexam.jp/",
+            "img": "banner_silver_06.png",
+        },
+        {
+            "url": "http://designegg.co.jp/",
+            "img": "banner_silver_07.png",
+        },
+    ],
+}
