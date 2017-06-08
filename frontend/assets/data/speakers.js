6201e19ba02f2237a4815c9fbe5163bedd9ad293
t.goto <t.goto@chatbox-inc.com>
2017-06-08 14:13:26 +0900

    update

--- frontend/assets/data/speakers.js
+++ frontend/assets/data/speakers.js
@@ -1,7 +1,15 @@
 module.exports = [
     {
-        name: "ショウノシオリ",
-        url: "https://chatbox-inc.com/member/shono/",
-        img: "staff_000.jpg",
+        track:"AAA",
+        time: "10:00",
+        name:"長谷川 智希",
+        "title": "",
+        "summary": "",
+        "profile": "",
+        "facebook": "",
+        "twitter": "",
+        "qiita": "",
+        "github": "",
+        "other": "",
     },
 ]
--- frontend/assets/data/staffs.js
+++ frontend/assets/data/staffs.js
@@ -1,15 +1,7 @@
 module.exports = [
     {
-        track:"AAA",
-        time: "10:00",
-        name:"長谷川 智希",
-        "title": "",
-        "summary": "",
-        "profile": "",
-        "facebook": "",
-        "twitter": "",
-        "qiita": "",
-        "github": "",
-        "other": "",
+        name: "ショウノシオリ",
+        url: "https://chatbox-inc.com/member/shono/",
+        img: "staff_000.jpg",
     },
 ]
