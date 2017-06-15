75fa66b791fbe200187ef6ac196e2e41383e7143
yasui05821 <yasui05821@gmail.com>
2017-06-15 17:53:21 +0900

    スタッフ情報修正　委員長バッジ追加

--- frontend/assets/data/staffs.js
+++ frontend/assets/data/staffs.js
@@ -20,73 +20,78 @@ module.exports = [
         img: "staff_default.png",
     },
     {
-        name: "田中ひさてる",
+        name: "tanakahisateru",
         url: "https://twitter.com/tanakahisateru",
         img: "staff_tanaka.jpg",
     },
     {
-        name: "おんさん",
+        name: "ONDX_jp",
         url: "https://www.facebook.com/daiki.onda.1",
         img: "staff_onda.jpg",
     },
     {
-        name: "川野奈緒",
+        name: "naokwn",
         url: "https://www.facebook.com/nkawano",
         img: "staff_kawano.jpg",
     },{
-        name: "有本雅哉",
+        name: "Arimoto",
         url: "https://www.facebook.com/masaya.arimoto",
         img: "staff_arimoto.jpg",
     },
+    // {
+    //     name: "原田さん",
+    //     url: "",
+    //     img: "staff_default.png",
+    // },
     {
-        name: "原田さん",
-        url: "",
-        img: "staff_default.png",
-    },
-    {
-        name: "高橋さん",
+        name: "yugo",
         url: "https://twitter.com/yugo_tak",
         img: "staff_takahashi.jpg",
     },
     {
-        name: "滝口さん",
+        name: "たきぐち(@atakig)",
         url: "https://twitter.com/atakig",
         img: "staff_takiguchi.jpeg",
     },
-    {
-        name: "佐田さん",
-        url: "",
-        img: "staff_default.png",
-    },
-    {
-        name: "筒井さん",
-        url: "",
-        img: "staff_default.png",
-    },
-    {
-        name: "大門さん",
-        url: "",
-        img: "staff_default.png",
-    },
-    {
-        name: "新原さん",
-        url: "",
-        img: "staff_default.png",
-    },
+    // {
+    //     name: "佐田さん",
+    //     url: "",
+    //     img: "staff_default.png",
+    // },
+    // {
+    //     name: "筒井さん",
+    //     url: "",
+    //     img: "staff_default.png",
+    // },
+    // {
+    //     name: "大門さん",
+    //     url: "",
+    //     img: "staff_default.png",
+    // },
+    // {
+    //     name: "新原さん",
+    //     url: "",
+    //     img: "staff_default.png",
+    // },
     {
         name: "どりぃ",
         url: "https://www.facebook.com/p526CA",
         img: "staff_dorii.jpg",
     },
-    {
-        name: "海恵",
-        url: "",
-        img: "staff_default.png",
-    },
+    // {
+    //     name: "海恵",
+    //     url: "",
+    //     img: "staff_default.png",
+    // },
     {
         name: "Yasui Risa",
         url: "https://www.facebook.com/yausi05821",
         img: "staff_yasui.jpg",
     },
+    // {
+    //     name: "榊原さん",
+    //     url: "",
+    //     img: "staff_default.png",
+    // },
 
 ]
